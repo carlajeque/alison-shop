75c42a5a7b9c2d66ffd18009c264a7ddecae4582
carlajeque <jequecarla@gmail.com>
2025-11-09 04:29:55 +0200

    Create page.jsx
    export default function Sent(){
      return (
        <div style={{padding:'20px 0'}}>
          <h2>✅ Pedido enviado!</h2>
          <p>Obrigado. Em breve entraremos em contato.</p>
        </div>
      )
    }

---
+++ public/public/public/public/styles/lib/locales/locales/locales/app/app/app/cart/app/vendors/app/vendors/sent/page.jsx
@@ -0,0 +1,8 @@
+export default function Sent(){
+  return (
+    <div style={{padding:'20px 0'}}>
+      <h2>✅ Pedido enviado!</h2>
+      <p>Obrigado. Em breve entraremos em contato.</p>
+    </div>
+  )
+}
